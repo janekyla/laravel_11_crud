425772e186b05685d31c907a057952d2b9036e7c
janekyla <JaneKylaRuben@outlook.com>
2025-05-17 11:02:40 +0800

    Add product image upload functionality and update product form

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\AuthController;
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/', [AuthController::class, 'login']);
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
