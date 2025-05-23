59ceb94f205b095d1f25acba7c5abe80541fdf9f
janekyla <JaneKylaRuben@outlook.com>
2025-05-23 14:40:51 +0800

    edit something

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,36 +8,31 @@
  Product Information
  </div>
  <div class="float-end">
- <a href="{{ route('products.index') }}" class="btn 
-btn-primary btn-sm">&larr; Back</a>
+ <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
  </div>
  </div>
  <div class="card-body">
  <div class="row">
- <label for="code" class="col-md-4 col-formlabel text-md-end text-start"><strong>Code:</strong></label>
- <div class="col-md-6" style="line-height:
-35px;">
+ <label for="code" class="col-md-4 col-form-label text-md-end text-start"><strong>Code:</strong></label>
+ <div class="col-md-6" style="line-height: 35px;">
  {{ $product->code }}
  </div>
  </div>
  <div class="row">
- <label for="name" class="col-md-4 col-formlabel text-md-end text-start"><strong>Name:</strong></label>
- <div class="col-md-6" style="line-height:
-35px;">
+ <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Name:</strong></label>
+ <div class="col-md-6" style="line-height: 35px;">
  {{ $product->name }}
  </div>
  </div>
  <div class="row">
- <label for="quantity" class="col-md-4 colform-label text-md-end text-start"><strong>Quantity:</strong></label>
- <div class="col-md-6" style="line-height:
-35px;">
+ <label for="quantity" class="col-md-4 col-form-label text-md-end text-start"><strong>Quantity:</strong></label>
+ <div class="col-md-6" style="line-height: 35px;">
  {{ $product->quantity }}
  </div>
  </div>
  <div class="row">
- <label for="price" class="col-md-4 col-formlabel text-md-end text-start"><strong>Price:</strong></label>
- <div class="col-md-6" style="line-height:
-35px;">
+ <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Price:</strong></label>
+ <div class="col-md-6" style="line-height:35px;">
  {{ $product->price }}
  </div>
  </div>
